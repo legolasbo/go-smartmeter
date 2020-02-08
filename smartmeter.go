16c9d5ef0bb5d6d1e353d11dc5b6d349ecc8ca25
legolasbo <legolasbo@2480548.no-reply.drupal.org>
2020-02-08 09:01:05 +0100

    Read line by line

--- smartmeter.go
+++ smartmeter.go
@@ -9,22 +9,56 @@ import (
 )
 
 func main() {
+		rChan := make(chan string)
+	tChan := make(chan string)
+	go readLines(rChan)
+	go collectTelegrams(rChan, tChan)
+	go parseTelegrams(tChan)
+}
+
+func readLines(rChan chan string) {
 	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 115200}
 	s, err := serial.OpenPort(c)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	i := 0
-	for (i < 10) {
-		reader := bufio.NewReader(s)
-		reply, err := reader.ReadBytes('/')
+	
+	reader := bufio.NewReader(s)
+	for {
+		reply, err := reader.ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
+			continue
 		}
-		fmt.Println("READ!")
-		fmt.Println(string(reply))
+	
+		rChan <- reply
+	}
+}
 
-		i++
+func collectTelegrams(rChan chan string, tChan chan string) {
+	telegram := ""
+	foundStart := false
+
+	for line := range rChan {
+		if line[0] == '/' {
+			foundStart = true
+			telegram = ""
+		}
+
+		if !foundStart {
+			continue
+		}
+
+		telegram += line;
+
+		if line[0] == '!' {
+			tChan <- telegram
+		}
+	}
+}
+
+func parseTelegrams(tChan chan string) {
+	for t := range tChan {
+		fmt.Println(t)
 	}
 }
