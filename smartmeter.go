c7a244c9f1e31b0c01490ccfc43f4814ff777065
legolasbo <legolasbo@2480548.no-reply.drupal.org>
2020-02-09 20:11:27 +0100

    Make go-smartmeter an actual package by removing the main logic.

--- smartmeter.go
+++ smartmeter.go
@@ -1,8 +1,7 @@
-package main
+package smartmeter
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 
 	"github.com/tarm/serial"
@@ -10,16 +9,21 @@ import (
 	dsmr "github.com/legolasbo/go-dsmr"
 )
 
-func main() {
-	rChan := make(chan string)
-	tChan := make(chan string)
-	go readLines(rChan)
-	go collectTelegrams(rChan, tChan)
-	parseTelegrams(tChan)
+// ReadTelegrams reads telegrams from the given serial port into the given telegram channel.
+func ReadTelegrams(serialPort string, tChan chan dsmr.Telegram) {
+	lineChan := make(chan string)
+	rawTelegramChan := make(chan string)
+	go readLines(serialPort, lineChan)
+	go collectTelegrams(lineChan, rawTelegramChan)
+	parseTelegrams(rawTelegramChan, tChan)
 }
 
-func readLines(rChan chan string) {
-	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 115200}
+func readLines(serialPort string, rChan chan string) {
+	c := &serial.Config{
+		Name: serialPort,
+		Baud: 115200,
+	}
+
 	s, err := serial.OpenPort(c)
 	if err != nil {
 		log.Fatal(err)
@@ -47,38 +51,30 @@ func collectTelegrams(rChan chan string, tChan chan string) {
 			telegram = ""
 		}
 
+		// We usually start halfway trough a telegram. 
+		// Which means that the first telegram would be corrupt.
+		// We therefore ignore everything until the first telegram start.
 		if !foundStart {
 			continue
 		}
 
 		telegram += line
 
+		// The last line of a telegram starts with an exclamation mark.
 		if line[0] == '!' {
 			tChan <- telegram
 		}
 	}
 }
 
-func parseTelegrams(tChan chan string) {
-	for t := range tChan {
+func parseTelegrams(rawTelegramChan chan string, tChan chan dsmr.Telegram) {
+	for t := range rawTelegramChan {
 		telegram, err := dsmr.ParseTelegram(t)
 		if err != nil {
-			fmt.Println(err)
 			log.Fatal(err)
 			continue
 		}
 
-		printTelegram(telegram)
+		tChan <- telegram
 	}
-}
-
-func printTelegram(t dsmr.Telegram) {
-	delivered, _ := t.ActualElectricityPowerDelivered()
-	fmt.Println("Actual electricity delivered", delivered, "kw")
-	received, _ := t.ActualElectricityPowerReceived()
-	fmt.Println("Actual electricity received", received, "kw")
-	gas1, _ := t.MeterReadingGasDeliveredToClient(1)
-	fmt.Println("Gas delivered on channel 1", gas1, "m3")
-	gas2, _ := t.MeterReadingGasDeliveredToClient(2)
-	fmt.Println("Gas delivered on channel 2", gas2, "m3")
 }
