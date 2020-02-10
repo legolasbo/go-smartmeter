91bad0bb2d44a60072a30d8c1f400cadd6c7f4b6
legolasbo <legolasbo@2480548.no-reply.drupal.org>
2020-02-10 20:54:21 +0100

    Add power delivery readouts

--- readout.go
+++ readout.go
@@ -19,7 +19,7 @@ func (r *Readout) Timestamp() time.Time {
 
 // PowerDelivered returns the kilowatts delivered to the grid in 1 watt resolution.
 func (r *Readout) PowerDelivered() float64 {
-	raw, ok := r.telegram.ActualElectricityPowerDelivered()
+	raw, ok := r.telegram.ActualElectricityPowerReceived()
 	if !ok {
 		return 0
 	}
@@ -29,7 +29,7 @@ func (r *Readout) PowerDelivered() float64 {
 
 // PowerReceived returns the kilowatts recieved from the grid in 1 watt resolution.
 func (r *Readout) PowerReceived() float64 {
-	raw, ok := r.telegram.ActualElectricityPowerReceived()
+	raw, ok := r.telegram.ActualElectricityPowerDelivered()
 	if !ok {
 		return 0
 	}
@@ -40,6 +40,46 @@ func (r *Readout) PowerReceived() float64 {
 // GasReceived returns the m3 of gas received from the mains in 1mm3 resolution.
 func (r *Readout) GasReceived(channel int) float64 {
 	raw, ok := r.telegram.MeterReadingGasDeliveredToClient(channel)
+	if !ok {
+		return 0
+	}
+	f, _ := strconv.ParseFloat(raw, 64)
+	return f
+}
+
+// TotalPowerReceivedLowTarif returns the total power received in the peak tarif in kWh with 1 Wh resolution.
+func (r *Readout) TotalPowerReceivedLowTarif() float64 {
+	raw, ok := r.telegram.MeterReadingElectricityDeliveredToClientTariff1()
+	if !ok {
+		return 0
+	}
+	f, _ := strconv.ParseFloat(raw, 64)
+	return f
+}
+
+// TotalPowerReceivedPeakTarif returns the total power received in the peak tarif in kWh with 1 Wh resolution.
+func (r *Readout) TotalPowerReceivedPeakTarif() float64 {
+	raw, ok := r.telegram.MeterReadingElectricityDeliveredToClientTariff2()
+	if !ok {
+		return 0
+	}
+	f, _ := strconv.ParseFloat(raw, 64)
+	return f
+}
+
+// TotalPowerDeliveredLowTarif returns the total power received in the peak tarif in kWh with 1 Wh resolution.
+func (r *Readout) TotalPowerDeliveredLowTarif() float64 {
+	raw, ok := r.telegram.MeterReadingElectricityDeliveredByClientTariff1()
+	if !ok {
+		return 0
+	}
+	f, _ := strconv.ParseFloat(raw, 64)
+	return f
+}
+
+// TotalPowerDeliveredPeakTarif returns the total power received in the peak tarif in kWh with 1 Wh resolution.
+func (r *Readout) TotalPowerDeliveredPeakTarif() float64 {
+	raw, ok := r.telegram.MeterReadingElectricityDeliveredByClientTariff2()
 	if !ok {
 		return 0
 	}
